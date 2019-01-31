f194dbdf456a1392a7d792a700ff83ec33cee9bb
ishaangulati8 <ishaangulati8@gmail.com>
2019-01-31 12:50:52 +0530

    Added sequelize and admin table

---
+++ models/admin.js
@@ -0,0 +1,22 @@
+'use strict';
+const bcrypt = require('bcrypt')
+module.exports = (sequelize, DataTypes) => {
+  const Admin = sequelize.define('Admin', {
+    adminName: DataTypes.STRING,
+    adminPassword: DataTypes.STRING
+  }, {});
+   Admin.beforeCreate( async(admin,options) => {
+    try{
+      if (admin.adminPassword) {
+        const hashPassword = await bcrypt.hash(admin.adminPassword,10);
+        admin.adminPassword = hashPassword;
+      }
+    } catch(error){
+      throw new Error(error);
+    }
+  })
+  Admin.associate = function(models) {
+    // associations can be defined here
+  };
+  return Admin;
+};
