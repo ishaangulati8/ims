1b770b0ba336a4acb5064408588074c692d3c810
ishaangulati8 <ishaangulati8@gmail.com>
2019-02-03 11:26:03 +0530

    Adding updated routes

--- routes/users/admin/index.js
+++ routes/users/admin/index.js
@@ -1,3 +1,10 @@
 const router = require('express').Router();
 const operator = require('./operator');
-const stockist = require('./')
+const stockist = require('./stockist');
+const product = require('./Products');
+
+router.use('/operator',operator);
+router.use('/stockist',stockist);
+router.use('/product',product);
+
+module.exports = router;
